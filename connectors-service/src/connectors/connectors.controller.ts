ef2103ec0a96a48e66db70d28cc0104caa914c28
Leroi Kakatsi <kingiscoding@gmail.com>
2025-11-03 23:25:02 +0100

    first project initialization

---
+++ connectors-service/src/connectors/connectors.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('connectors')
+export class ConnectorsController {}
---
+++ connectors-service/src/widgets/widgets.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { WidgetsService } from './widgets.service';
+import { WidgetsController } from './widgets.controller';
+
+@Module({
+  providers: [WidgetsService],
+  controllers: [WidgetsController]
+})
+export class WidgetsModule {}
